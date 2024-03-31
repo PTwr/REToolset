94bbaa7e7777c34fc2aa93f2f6997433f1066df3
PTwr <PTwr@outlook.com>
2024-03-31 01:01:00 +0100

    Simplified Fluent Object Deserializer detached from limitations of object properties

---
+++ BinaryFile/BinaryFile.Tests/FluentDeserializerTests.cs
@@ -0,0 +1,64 @@
+﻿using BinaryFile.Unpacker.Deserializers;
+using BinaryFile.Unpacker.Metadata;
+using BinaryFile.Unpacker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryFile.Tests
+{
+    public class FluentDeserializerTests
+    {
+        class FlatPOCO
+        {
+            public byte A { get; set; }
+            public byte B { get; set; }
+            public byte C { get; set; }
+            public byte D { get; set; }
+        }
+        [Fact]
+        public void FlatPOCOTest()
+        {
+            var bytes = new byte[] {
+                1, 2, 3, 4,
+            };
+
+            var ctx = new RootDataOffset(new DeserializerManager());
+
+            var fluentDeserializer = new FluentDeserializer<FlatPOCO>();
+
+            //TODO setter from getter in first invocation
+            //TODO add Expression overloads to get nice ToString description. Will require getter->setter transformation as embedeed Expresssion can't contain assignment as of yet.
+            fluentDeserializer
+                .WithField<byte>("A")
+                .AtOffset(0)
+                .Into((poco, b) => poco.A = b);
+            fluentDeserializer
+                .WithField<byte>("B")
+                .AtOffset(1)
+                .Into((poco, b) => poco.B = b);
+            fluentDeserializer
+                .WithField<byte>("C")
+                .AtOffset(2)
+                .Into((poco, b) => poco.C = b);
+            fluentDeserializer
+                .WithField<byte>("D")
+                .AtOffset(3)
+                .Into((poco, b) => poco.D = b);
+
+            ctx.Manager.Register(fluentDeserializer);
+            ctx.Manager.Register(new IntegerDeserializer());
+
+            var result = fluentDeserializer.Deserialize(bytes, out var success, ctx, out var consumedLength);
+            Assert.NotNull(result);
+            Assert.True(success);
+
+            Assert.Equal(1, result.A);
+            Assert.Equal(2, result.B);
+            Assert.Equal(3, result.C);
+            Assert.Equal(4, result.D);
+        }
+    }
+}
--- BinaryFile/BinaryFile.Unpacker/Deserializers/FluentDeserializer.cs
+++ BinaryFile/BinaryFile.Unpacker/Deserializers/FluentDeserializer.cs
@@ -5,28 +5,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BinaryFile.Unpacker.Deserializers
 {
-    public class FluentDeserializer<TDeclaringType> : SerializedAndDeserializerBase<TDeclaringType>
+    public class FluentDeserializer<TDeclaringType> : SerializedAndDeserializerBase<TDeclaringType>, IDeserializer<TDeclaringType>
     {
         List<IFluentFieldDescriptor<TDeclaringType>> Descriptors = new List<IFluentFieldDescriptor<TDeclaringType>>();
 
         public TDeclaringType Deserialize(Span<byte> data, out bool success, DeserializationContext deserializationContext, out int consumedLength)
         {
-            throw new NotImplementedException();
+            consumedLength = 0;
+            success = false;
+            //TODO safety checks
+            var result = (TDeclaringType)Activator.CreateInstance(MappedType);
+
+            foreach (var descriptor in Descriptors)
+            {
+                descriptor.TryDeserialize(data, result, deserializationContext, out consumedLength);
+            }
+
+            success = true;
+            return result;
         }
 
-        public FluentFieldDescriptor<TDeclaringType, TItem> WithField<TItem>()
+        public FluentFieldDescriptor<TDeclaringType, TItem> WithField<TItem>(string? name = null)
         {
-            var descriptor = new FluentFieldDescriptor<TDeclaringType, TItem>();
+            var descriptor = new FluentFieldDescriptor<TDeclaringType, TItem>(name);
             Descriptors.Add(descriptor);
             return descriptor;
         }
 
-        public FluentCollectionDescriptor<TDeclaringType, TItem> WithCollectionOf<TItem>()
+        public FluentCollectionDescriptor<TDeclaringType, TItem> WithCollectionOf<TItem>(string? name = null)
         {
-            var descriptor = new FluentCollectionDescriptor<TDeclaringType, TItem>();
+            var descriptor = new FluentCollectionDescriptor<TDeclaringType, TItem>(name);
             Descriptors.Add(descriptor);
             return descriptor;
         }
@@ -39,11 +51,22 @@ namespace BinaryFile.Unpacker.Deserializers
     public abstract class _BaseFluentFieldDescriptor<TDeclaringType, TItem, TImplementation> : IFluentFieldDescriptor<TDeclaringType>
         where TImplementation : _BaseFluentFieldDescriptor<TDeclaringType, TItem, TImplementation>
     {
+        public _BaseFluentFieldDescriptor(string? name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name ?? base.ToString()!;
+        }
+
         public OffsetRelation OffsetRelation { get; protected set; }
         public FuncField<TDeclaringType, int>? Offset { get; protected set; }
         public FuncField<TDeclaringType, int>? Length { get; protected set; }
         public FuncField<TDeclaringType, Encoding>? Encoding { get; protected set; }
         public FuncField<TDeclaringType, bool>? IsNestedFile { get; protected set; }
+        public string? Name { get; protected set; }
 
         public TImplementation AtOffset(int offset, OffsetRelation offsetRelation = OffsetRelation.Segment)
         {
@@ -101,8 +124,34 @@ namespace BinaryFile.Unpacker.Deserializers
 
         public abstract bool TryDeserialize(Span<byte> bytes, TDeclaringType declaringObject, DeserializationContext deserializationContext, out int consumedLength);
     }
+    public class FluentFieldContext<TDeclaringType, TItem> : DeserializationContext
+    {
+        private readonly FluentFieldDescriptor<TDeclaringType, TItem> fieldDescriptor;
+        private readonly TDeclaringType declaringObject;
+
+        public override int? Length => fieldDescriptor.Length?.Get(declaringObject);
+        public override Encoding? Encoding => fieldDescriptor.Encoding?.Get(declaringObject);
+
+        public FluentFieldContext(DeserializationContext? parent, OffsetRelation offsetRelation, int relativeOffset, FluentFieldDescriptor<TDeclaringType, TItem> fieldDescriptor, TDeclaringType declaringObject)
+            : base(parent, offsetRelation, relativeOffset)
+        {
+            this.fieldDescriptor = fieldDescriptor;
+            this.declaringObject = declaringObject;
+        }
+
+        //short circuit Absolute for nested file
+        //TODO clean rewrite, this is fugly :D
+        public override DeserializationContext Find(OffsetRelation offsetRelation) => (fieldDescriptor.IsNestedFile?.Get(declaringObject) ?? false) ? (
+            offsetRelation is OffsetRelation.Absolute or OffsetRelation.Segment ? this :
+                throw new ArgumentException($"{fieldDescriptor}. Looking for ancestor DataOffset of NestedFile. Remaining OffsetRelation = {offsetRelation}"))
+            : base.Find(offsetRelation);
+    }
     public class FluentFieldDescriptor<TDeclaringType, TItem> : _BaseFluentFieldDescriptor<TDeclaringType, TItem, FluentFieldDescriptor<TDeclaringType, TItem>>
     {
+        public FluentFieldDescriptor(string? name) : base(name)
+        {
+        }
+
         public Action<TDeclaringType, TItem>? Setter { get; protected set; }
         public FluentFieldDescriptor<TDeclaringType, TItem> Into(Action<TDeclaringType, TItem> setter)
         {
@@ -112,11 +161,35 @@ namespace BinaryFile.Unpacker.Deserializers
 
         public override bool TryDeserialize(Span<byte> bytes, TDeclaringType declaringObject, DeserializationContext deserializationContext, out int consumedLength)
         {
-            throw new NotImplementedException();
+            if (Setter == null) throw new Exception($"{this}. Setter has not been provided!");
+
+            consumedLength = 0;
+            if (declaringObject == null) return false;
+
+            TItem v = default!;
+
+            int relativeOffset = Offset?.Get(declaringObject) ?? throw new Exception($"{this}. Neither Offset nor OffsetFunc has been set!");
+
+            var ctx = new FluentFieldContext<TDeclaringType, TItem>(deserializationContext, OffsetRelation, relativeOffset, this, declaringObject);
+
+            if (deserializationContext.Manager.TryGetMapping<TItem>(out var deserializer) is false) return false;
+
+            v = deserializer.Deserialize(bytes, out var success, ctx, out consumedLength);
+
+            if (success)
+            {
+                Setter(declaringObject, v);
+
+                return true;
+            }
+            return false;
         }
     }
     public class FluentCollectionDescriptor<TDeclaringType, TItem> : _BaseFluentFieldDescriptor<TDeclaringType, TItem, FluentCollectionDescriptor<TDeclaringType, TItem>>
     {
+        public FluentCollectionDescriptor(string? name) : base(name)
+        {
+        }
 
         public FuncField<TDeclaringType, int>? Count { get; protected set; }
         public FuncField<TDeclaringType, int>? ItemLength { get; protected set; }
