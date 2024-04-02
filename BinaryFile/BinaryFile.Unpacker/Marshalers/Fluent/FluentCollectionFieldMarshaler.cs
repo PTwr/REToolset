78db226ee71239df4474d824f62cba5d80d56297
PTwr <PTwr@outlook.com>
2024-04-02 22:36:20 +0200

    fluent collection deserialization body

--- BinaryFile/BinaryFile.Unpacker/Marshalers/Fluent/FluentCollectionFieldMarshaler.cs
+++ BinaryFile/BinaryFile.Unpacker/Marshalers/Fluent/FluentCollectionFieldMarshaler.cs
@@ -27,9 +27,70 @@ namespace BinaryFile.Unpacker.Marshalers.Fluent
 
         protected Func<TDeclaringType, IEnumerable<TItem>, bool>? BreakWhenFunc { get; set; }
 
-        public void Deserialize(TDeclaringType declaringObject, Span<byte> bytes, IMarshalingContext deserializationContext, out int consumedLength)
+        public void Deserialize(TDeclaringType declaringObject, Span<byte> bytes, IMarshalingContext context, out int consumedLength)
         {
-            throw new NotImplementedException();
+            if (Setter == null) throw new Exception($"{this}. Setter has not been provided!");
+
+            consumedLength = 0;
+            if (declaringObject == null) throw new ArgumentException($"{Name}. Declaring object is required for Fluent Deserialization!");
+
+            var availableBytes = context.Slice(bytes).Length;
+            int maxAbsoluteItemOffset = context.AbsoluteOffset + availableBytes;
+
+            List<KeyValuePair<int, TItem>> Items = new List<KeyValuePair<int, TItem>>();
+
+            int itemOffsetCorrection = 0;
+            for (int itemNumber = 0; ; itemNumber++)
+            {
+                if (Metadata.Count is not null && itemNumber >= Metadata.Count.Get(declaringObject)) break;
+
+                int collectionRelativeOffset = Offset?.Get(declaringObject) ?? throw new Exception($"{Name}.Neither Offset nor OffsetFunc has been set!");
+                var itemRelativeOffset = collectionRelativeOffset + itemOffsetCorrection;
+                var itemContext = new FluentMarshalingContext<TDeclaringType, TItem>(Name, context, OffsetRelation, itemRelativeOffset, Metadata, declaringObject);
+
+                if (itemContext.AbsoluteOffset > bytes.Length)
+                    throw new Exception($"{Name}. Absolute offset of {itemContext.AbsoluteOffset} is larger than dataset of {bytes.Length} bytes.");
+
+                if (itemContext.AbsoluteOffset > maxAbsoluteItemOffset)
+                    throw new Exception($"{Name}. Item offset of {itemOffsetCorrection} (abs {itemContext.AbsoluteOffset}) for item #{itemNumber} exceedes limits of field slice of {availableBytes} bytes.");
+
+                if (context.DeserializerManager.TryGetMapping<TItem>(out var deserializer) is false) throw new Exception($"{Name}. Deserializer for {typeof(TItem).FullName} not found.");
+
+                var item = deserializer.Deserialize(bytes, itemContext, out consumedLength);
+
+                if (Metadata.ItemLength is not null) consumedLength = Metadata.ItemLength.Get(declaringObject);
+
+                //TODO add option to disable this error?
+                if (consumedLength <= 0) throw new Exception($"{Name}. Non-positive item consumed byte length of {consumedLength}!");
+
+                //TODO error on offset dups? but previous check should prevent dups
+                Items.Add(new KeyValuePair<int, TItem>(itemOffsetCorrection, item));
+
+                itemOffsetCorrection += consumedLength;
+            }
+
+            Validate(declaringObject, Items.Select(i => i.Value));
+
+            //TODO alternative Setters accepting (offset, value) pairs (GEV/OFS will need that)
+            Setter(declaringObject, Items.Select(i => i.Value));
+        }
+
+        //TODO rewrite! fugly!
+        public void Validate(TDeclaringType declaringObject, IEnumerable<TItem> values)
+        {
+            if (ExpectedValue is not null)
+            {
+                var expectedVal = ExpectedValue.Get(declaringObject);
+                var result = expectedVal is null ? values == null : expectedVal.SequenceEqual(values);
+                //TODO figure out better exception for displaying items :D
+                if (!result) throw new Exception($"{Name}. Unexpected Value! Expected: '{expectedVal}', actual: '{values}'");
+            }
+            if (ValidateFunc is not null)
+            {
+                var result = ValidateFunc.Invoke(declaringObject, values);
+                //TODO figure out better exception for displaying items :D
+                if (!result) throw new Exception($"{Name}. Validation failed! Deserialized values: '{values}'");
+            }
         }
 
         public void Serialize(TDeclaringType declaringObject, ByteBuffer buffer, ISerializationContext serializationContext, out int consumedLength)
