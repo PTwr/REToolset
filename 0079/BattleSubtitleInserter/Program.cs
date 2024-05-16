e06db108daece785a45aa93116deb91c8068b29c
PTwr <PTwr@outlook.com>
2024-05-17 00:02:56 +0200

    Insert battle-subs into EVE Prefetch block

--- 0079/BattleSubtitleInserter/Program.cs
+++ 0079/BattleSubtitleInserter/Program.cs
@@ -1,4 +1,13 @@
-﻿namespace BattleSubtitleInserter
+﻿using BinaryDataHelper;
+using BinaryFile.Formats.Nintendo;
+using BinaryFile.Formats.Nintendo.R79JAF;
+using BinaryFile.Formats.Nintendo.R79JAF.GEV;
+using BinaryFile.Marshaling.Context;
+using BinaryFile.Marshaling.MarshalingStore;
+using BinaryFile.Marshaling.TypeMarshaling;
+using System.Text;
+
+namespace BattleSubtitleInserter
 {
     internal class Program
     {
@@ -10,7 +19,71 @@
             //TODO find voice invocation in EVE
             //TODO get actor from voice invocation
             //TODO generate Subtitle brres/arc with matching face
-            Console.WriteLine("Hello, World!");
+
+            var ctx = PrepXBFMarshaling(out var mXBF, out var mU8, out var mGEV);
+
+            var gevTutprial = Directory.EnumerateFiles(@"C:\G\Wii\R79JAF_clean\DATA\files\event\missionevent\other", "TR*.gev");
+            var gevAce = Directory.EnumerateFiles(@"C:\G\Wii\R79JAF_clean\DATA\files\event\missionevent\ace", "A*.gev");
+            var gevEarth = Directory.EnumerateFiles(@"C:\G\Wii\R79JAF_clean\DATA\files\event\missionevent\efsf", "me*.gev");
+            var gevZeon = Directory.EnumerateFiles(@"C:\G\Wii\R79JAF_clean\DATA\files\event\missionevent\zeon", "mz*.gev");
+
+            var voiceFiles = Directory.EnumerateFiles(@"C:\G\Wii\R79JAF_clean\DATA\files\sound\stream", "*.brstm")
+                .Select(i => Path.GetFileNameWithoutExtension(i)).ToHashSet();
+
+            foreach (var file in gevAce)
+            {
+                //HACK test on AA01 only for now
+                if (!file.Contains("AA01")) continue;
+
+                var gev = mGEV.Deserialize(null, null, File.ReadAllBytes(file).AsMemory(), ctx, out _);
+
+                var referencedVoiceFiles = gev.STR
+                    .Select((s, n) => new { s, n })
+                    .Where(i => voiceFiles.Contains(i.s)).ToList();
+
+                var prefetchLine = gev.EVESegment.Blocks[1].EVELines[0];
+
+                foreach(var refVoice in referencedVoiceFiles)
+                {
+                    var sbytes = refVoice.s.ToBytes(Encoding.ASCII, fixedLength: 8);
+
+                    prefetchLine.Body.InsertRange(1, [
+                            //cutin/avatar load
+                            new EVEOpCode(0x00A4FFFF),
+                            //string
+                            new EVEOpCode(sbytes.Take(4)),
+                            new EVEOpCode(sbytes.Skip(4).Take(4)),
+                            //load cutin only
+                            new EVEOpCode(0x00000001),
+                        ]);
+
+                    prefetchLine.LineLengthOpCode.HighWord += 4;
+                }
+
+                var outputFile = file.Replace("_clean", "_dirty");
+                var bb = new ByteBuffer();
+                mGEV.Serialize(gev, bb, ctx, out _);
+                File.WriteAllBytes(outputFile, bb.GetData());
+            }
+        }
+
+        private static IMarshalingContext PrepXBFMarshaling(out ITypeMarshaler<XBFFile> mXBF, out ITypeMarshaler<U8File> mU8
+            , out ITypeMarshaler<GEV> mGEV)
+        {
+            var store = new DefaultMarshalerStore();
+            var rootCtx = new RootMarshalingContext(store);
+
+            U8Marshaling.Register(store);
+            XBFMarshaling.Register(store);
+            GEVMarshaling.Register(store);
+
+            mXBF = store.FindMarshaler<XBFFile>();
+
+            mU8 = store.FindMarshaler<U8File>();
+
+            mGEV = store.FindMarshaler<GEV>();
+
+            return rootCtx;
         }
     }
 }
