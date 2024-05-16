388e60924baa1f730fd5e4114f91b7d2d8d47abe
PTwr <PTwr@outlook.com>
2024-05-17 01:50:06 +0200

    patch all gevs!

--- 0079/BattleSubtitleInserter/Program.cs
+++ 0079/BattleSubtitleInserter/Program.cs
@@ -2,9 +2,13 @@
 using BinaryFile.Formats.Nintendo;
 using BinaryFile.Formats.Nintendo.R79JAF;
 using BinaryFile.Formats.Nintendo.R79JAF.GEV;
+using BinaryFile.Formats.Nintendo.R79JAF.GEV.EVECommands;
 using BinaryFile.Marshaling.Context;
 using BinaryFile.Marshaling.MarshalingStore;
 using BinaryFile.Marshaling.TypeMarshaling;
+using ConcurrentCollections;
+using R79JAFshared;
+using System.Collections.Concurrent;
 using System.Text;
 
 namespace BattleSubtitleInserter
@@ -20,6 +24,13 @@ namespace BattleSubtitleInserter
             //TODO get actor from voice invocation
             //TODO generate Subtitle brres/arc with matching face
 
+            var gen = new SubtitleImgCutInGenerator(
+                @"C:\G\Wii\R79JAF patch assets\SubtitleAssets",
+                @"C:\G\Wii\R79JAF_dirty\DATA\files\sound\stream",
+                @"C:\G\Wii\R79JAF patch assets\subtitleTranslation",
+                @"C:\G\Wii\R79JAF patch assets\tempDir"
+                );
+
             var ctx = PrepXBFMarshaling(out var mXBF, out var mU8, out var mGEV);
 
             var gevTutprial = Directory.EnumerateFiles(@"C:\G\Wii\R79JAF_clean\DATA\files\event\missionevent\other", "TR*.gev");
@@ -30,10 +41,15 @@ namespace BattleSubtitleInserter
             var voiceFiles = Directory.EnumerateFiles(@"C:\G\Wii\R79JAF_clean\DATA\files\sound\stream", "*.brstm")
                 .Select(i => Path.GetFileNameWithoutExtension(i)).ToHashSet();
 
-            foreach (var file in gevAce)
+            var allGevs = gevTutprial.Concat(gevAce).Concat(gevEarth).Concat(gevZeon);
+
+            ConcurrentHashSet<string> processedCutIns = new ConcurrentHashSet<string>();
+
+            //foreach (var file in allGevs)
+            Parallel.ForEach(allGevs, (file) =>
             {
                 //HACK test on AA01 only for now
-                if (!file.Contains("AA01")) continue;
+                //if (!file.Contains("AK01", StringComparison.InvariantCultureIgnoreCase)) continue;
 
                 var gev = mGEV.Deserialize(null, null, File.ReadAllBytes(file).AsMemory(), ctx, out _);
 
@@ -43,7 +59,7 @@ namespace BattleSubtitleInserter
 
                 var prefetchLine = gev.EVESegment.Blocks[1].EVELines[0];
 
-                foreach(var refVoice in referencedVoiceFiles)
+                foreach (var refVoice in referencedVoiceFiles)
                 {
                     var sbytes = refVoice.s.ToBytes(Encoding.ASCII, fixedLength: 8);
 
@@ -60,11 +76,47 @@ namespace BattleSubtitleInserter
                     prefetchLine.LineLengthOpCode.HighWord += 4;
                 }
 
+                foreach (var line in gev.EVESegment.Blocks.SelectMany(i => i.EVELines))
+                {
+                    var parsedCommands = line.ParsedCommands;
+
+                    var voicePlaybacks = parsedCommands.OfType<VoicePlayback>();
+
+                    foreach (var voicePlayback in voicePlaybacks)
+                    {
+                        Console.WriteLine(voicePlayback.Str);
+                        var index = parsedCommands.IndexOf(voicePlayback);
+
+                        index++;
+                        if (index < parsedCommands.Count && parsedCommands[index] is AvatarDisplay avatar)
+                        {
+                            var sbytes = voicePlayback.Str.ToBytes(Encoding.ASCII, fixedLength: 8);
+
+                            Console.WriteLine(avatar.Str);
+
+                            var pilotCodeOverride = avatar.Str.Replace("\x0", "");
+                            //do not do face match for minions
+                            if (pilotCodeOverride.StartsWith("sl", StringComparison.InvariantCultureIgnoreCase))
+                                pilotCodeOverride = null;
+                            
+                            //do not process same file multiple times, one voice file = one avatar
+                            if(processedCutIns.Add(voicePlayback.Str))
+                                gen.RepackSubtitleTemplate(voicePlayback.Str, @"C:\G\Wii\R79JAF_dirty\DATA\files\_2d\ImageCutIn", pilotCodeOverride);
+
+                            //update avatar.Str to match voice.Str
+                            line.Body[avatar.Pos + 1] = new EVEOpCode(sbytes.Take(4));
+                            line.Body[avatar.Pos + 2] = new EVEOpCode(sbytes.Skip(4).Take(4));
+                            //display ImgCutIn instead of MsgBox
+                            line.Body[avatar.Pos + 3] = new EVEOpCode(0x0002FFFF);
+                        }
+                    }
+                }
+
                 var outputFile = file.Replace("_clean", "_dirty");
                 var bb = new ByteBuffer();
                 mGEV.Serialize(gev, bb, ctx, out _);
                 File.WriteAllBytes(outputFile, bb.GetData());
-            }
+            });
         }
 
         private static IMarshalingContext PrepXBFMarshaling(out ITypeMarshaler<XBFFile> mXBF, out ITypeMarshaler<U8File> mU8
