cd10c84d5c1b3be382b0fc79c5c49bc79f6c63fb
PTwr <PTwr@outlook.com>
2025-02-12 14:51:14 +0100

    using trim

--- BinaryFile/BinaryFile.MarshalingDI.Tests/CollectionMarshaling.cs
+++ BinaryFile/BinaryFile.MarshalingDI.Tests/CollectionMarshaling.cs
@@ -12,9 +12,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static BinaryFile.MarshalingDI.Tests.CollectionMarshaling;
-using static System.Formats.Asn1.AsnWriter;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BinaryFile.MarshalingDI.Tests
 {
