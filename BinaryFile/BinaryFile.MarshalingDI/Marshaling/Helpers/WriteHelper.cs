9c37b655bed4bacf36a50bff20fbb4bbcbd8e074
PTwr <PTwr@outlook.com>
2025-05-03 02:34:47 +0200

    fix for variance when fetching WriteMarshalers

--- BinaryFile/BinaryFile.MarshalingDI/Marshaling/Helpers/WriteHelper.cs
+++ BinaryFile/BinaryFile.MarshalingDI/Marshaling/Helpers/WriteHelper.cs
@@ -22,7 +22,22 @@ namespace BinaryFile.MarshalingDI.Marshaling.Helpers
 
         public void Write<T>(T value, out int bytesWrote)
         {
-            if (marshalerStore.TryGetWriteMarshaler<T>(value, out var writer))
+            if (value is null)
+            {
+                bytesWrote = 0;
+                return;
+            }
+
+            var mi = typeof(WriteHelper).GetMethod(nameof(_Write), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var gmi = mi!.MakeGenericMethod(value.GetType());
+
+            object?[] p = [value, null];
+            gmi.Invoke(this, p);
+            bytesWrote = (int)p[1]!;
+        }
+        protected void _Write<TExact>(TExact value, out int bytesWrote)
+        {
+            if (marshalerStore.TryGetWriteMarshaler<TExact>(value, out var writer))
             {
                 try
                 {
@@ -33,7 +48,7 @@ namespace BinaryFile.MarshalingDI.Marshaling.Helpers
                     throw new Exception($"An exception has occured while Writing value '{value?.ToString()}'. {features.GetDebugInfo()}", ex);
                 }
             }
-            else throw new InvalidOperationException($"No Write marshaler found for {typeof(T).FullName}. {features.GetDebugInfo()}");
+            else throw new InvalidOperationException($"No Write marshaler found for {typeof(TExact).FullName}. {features.GetDebugInfo()}");
 
             var padding = features.Find<IPadding>(null, 0);
             padding?.GetValue()?.Pad(bytesWrote, out bytesWrote);
